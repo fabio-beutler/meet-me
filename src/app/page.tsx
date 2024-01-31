2d1ea3c2306d73ac854c2c50fcc6af1f72b8da7b
Fabio Beutler <dev.fabiobeutler@gmail.com>
2024-01-30 23:38:39 -0400

    Update global CSS and font in layout and page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <main className="">
+    <main>
       <h1>hello world</h1>
+      <Button>test button</Button>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,7 @@ const config = {
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
       fontFamily: {
-        sans: ['var(--inter)', ...fontFamily.sans],
+        sans: ['var(--roboto)', ...fontFamily.sans],
       },
     },
   },
