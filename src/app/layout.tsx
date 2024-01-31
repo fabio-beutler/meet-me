c8514bf4a3bf57114897786f8ff561e30e7910c1
Fabio Beutler <dev.fabiobeutler@gmail.com>
2024-01-30 23:24:07 -0400

    Update layout and page components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'], variable: '--inter' });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans text-foreground antialiased',
+          inter.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
