92694881b3e5ce31099151583b5a41b20747ddd3
Fabio Beutler <dev.fabiobeutler@gmail.com>
2024-01-30 23:12:01 -0400

    Add new components and update settings and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/components/ui/button';
+import { Loader2 } from 'lucide-react';
+
 export default function Home() {
   return (
     <main className="">
       <h1>hello world</h1>
+      <Button>
+        button test <Loader2 className="animate-spin" />
+      </Button>
     </main>
   );
 }
