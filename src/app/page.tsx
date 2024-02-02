002dcb90ad0a2df4f725f4001d5cd050a74b0e5a
Fabio Beutler <dev.fabiobeutler@gmail.com>
2024-02-01 23:11:35 -0400

    Rearranged heading font size in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="ml-auto flex h-screen max-w-[calc(100vw-(100vw-1160px)/2)] items-center gap-20 overflow-hidden">
       <div className="max-w-[580px] px-10">
-        <h1 className="sm:text-7.3xl text-5xl font-extrabold">
+        <h1 className="text-4xl font-extrabold sm:text-7.3xl">
           Agendamento descomplicado
         </h1>
         <p className="mt-2 text-xl leading-relaxed text-muted-foreground">
