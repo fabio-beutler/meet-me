cd82da3e3ddc40a6f3a83ca803054a5408a171b4
Fabio Beutler <dev.fabiobeutler@gmail.com>
2024-02-15 23:58:29 -0400

    ✨ feat(src/app/providers): Add React import to Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@
 import { setDefaultOptions } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { SessionProvider } from 'next-auth/react';
+import React from 'react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   setDefaultOptions({ locale: ptBR });
