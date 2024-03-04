1b0629700c1b7446546a6cad8d83562f29ba68e4
Fabio Beutler <dev.fabiobeutler@gmail.com>
2024-03-04 08:31:27 -0400

    chore: add @tanstack/react-query

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import { QueryClientProvider } from '@tanstack/react-query';
 import { setDefaultOptions } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 
 import { ThemeProvider } from '@/components/theme-provider';
+import { queryClient } from '@/lib/react-query';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   setDefaultOptions({ locale: ptBR });
@@ -16,7 +18,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
       enableSystem
       disableTransitionOnChange
     >
-      <SessionProvider>{children}</SessionProvider>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </SessionProvider>
     </ThemeProvider>
   );
 }
